9fa024c21ab260bae716a7213b0178063fbf1411
MonoHue <libertyli@hotmail.com>
2021-01-01 00:53:32 +0800

    Initial Update.

---
+++ cmake-build-debug/CMakeFiles/Melchior.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Melchior.dir/LUOGU_P1001.cpp.o"
+  "Melchior"
+  "Melchior.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Melchior.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
