8ac82e016cc9d42d0bfdd78461525015bebc510b
MonoHue <libertyli@hotmail.com>
2021-03-11 15:49:00 +0800

    AC POJ_2229.cpp

--- POJ/POJ_2229.cpp
+++ POJ/POJ_2229.cpp
@@ -1,30 +1,34 @@
+/*
+ * dp[i] = dp[i - 1]    (i is odd)
+ * dp[i] = dp[i - 1] + dp[i / 2]    (i is even)
+ *
+ * 1.   When i is odd, attach '1' to each expression. Thus, the cases remain the same.
+ * 2.   When i is even,
+ *  a)  When a expression does not have '1', we divide each number with 2, and the quantity of cases is dp[i / 2].
+ *  b)  Else, if a expression has '1', attach '1' to each expression. the quantity of cases is dp[i - 1].
+ */
+
+
 #include <iostream>
+#define MOD 1000000000
 using namespace std;
 
-long long n, ans = 0;
-long long twopowers[40];
-
-//TODO: Algorithm Design
-long long solve(long long a)   {
-    ans += solve(a);
-
-}
-
-
+int n;
+int dp[1000010];
 
 int main()  {
     cin >> n;
+    dp[0] = 0;dp[1] = 1;
 
-    int temp = 1;
-    for(int i = 0;temp < 1000000;i++)   {
-        twopowers[i] = temp;
-        temp *= 2;
-        cout << twopowers[i] << " ";
+    for(int i = 2;i <= n;i++)   {
+        if(i & 1)   dp[i] = dp[i - 1];
+        else {
+            dp[i] = dp[i - 1] + dp[i / 2];
+            dp[i] %= MOD;
+        }
     }
 
-    //solve(n);
-
-    cout << ans;
+    cout << dp[n];
 
     return 0;
 }
